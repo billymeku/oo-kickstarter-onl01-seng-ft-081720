ea8ca1a14e51eb8a97a85dd8a87f363649cc1a27
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-02 07:17:53 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,15 @@
 class Backer 
-  attr_reader :name, :backed_projects
+  attr_reader :name
+  attr_accessor :backed_projects
   
    def initialize(name)
      @name = name 
    @backed_projects = []
     end 
   
-  def back_project(backers)
-    @back_projects << backers
+  def back_project(project)
+    @back_projects << project
+    project.
     
   end 
   

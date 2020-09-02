ecc1f90fda06bc61f4006322649ec5afad98bb6f
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-02 06:47:43 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,12 @@
-# class Project 
-#   attr_reader :title, :backed_projects
+class Project 
+  attr_reader :title, :backed_projects
   
   
-#   def initialize(title)
-#   @title = title
-#   @backed_projects = []
+  def initialize(title)
+  @title = title
+  @backed_projects = []
     
-#   end 
+  end 
   
   
-# end 
+end 
